829de289f8b9ccb155e69075bb51855e8cc20c26
Tiago Custodio <tiagoluis86@outlook.com>
2023-05-26 14:19:41 -0300

    UPDATE busca por filmes e ADD biblioteca

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class SelectedMovie(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    release_date = models.DateField()
+    average_rating = models.FloatField()
+    user_rating = models.FloatField()
+
+    def __str__(self):
+        return self.title
--- core/views.py
+++ core/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render
-
-# Create your views here.
-
-from django.db import models
-from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 import requests
+
+API_KEY = '1e440f65016c5cbd7a4d5712968422a2'
 
 @login_required
 def homepage(request):
@@ -15,11 +12,10 @@ def homepage(request):
 def search_movie(request):
     if request.method == 'POST':
         search_query = request.POST.get('search_query')
-        print(search_query)
-        api_key = '1e440f65016c5cbd7a4d5712968422a2'  # Substitua por sua chave de API do The Movie DB
+        print(search_query)   
 
         # Realize a chamada à API do The Movie DB com o termo de busca
-        response = requests.get(f'https://api.themoviedb.org/3/search/movie?api_key={api_key}&query={search_query}')
+        response = requests.get(f'https://api.themoviedb.org/3/search/movie?api_key={API_KEY}&query={search_query}')
         data = response.json()
 
         print(data)
@@ -31,6 +27,25 @@ def search_movie(request):
             results = []
 
         context = {'results': results, 'search_query': search_query}
+        return render(request, 'main.html', context)
+    
+    if request.method == 'GET' and 'selected_movies' in request.GET:
+        selected_movie_ids = request.GET.getlist('selected_movies')
+        selected_movies = []
+        for movie_id in selected_movie_ids:
+            # Realize uma chamada à API do The Movie DB para obter os detalhes do filme
+            response = requests.get(f'https://api.themoviedb.org/3/movie/{movie_id}?api_key={API_KEY}')
+            movie_data = response.json()
+
+            movie = {
+                'title': movie_data['title'],
+                'release_date': movie_data['release_date'],
+                'average_rating': movie_data['vote_average'],
+                'user_rating': 0,  # Você precisará substituir pelo valor real da nota do usuário para o filme
+            }
+            selected_movies.append(movie)
+
+        context = {'selected_movies': selected_movies}
         return render(request, 'main.html', context)
 
     return render(request, 'main.html')
